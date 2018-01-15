02ea7b86f3ae3f5971c50aba5e1cd3a3c5f33308
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-15 00:05:52 -0500

    requires npm packages

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+//=====================COMMAND LINE FUNCTIONS======================
+
+//Requiring npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var table = require("console.table"); 
+
+//Establishing connection
+
+console.table([
+  {//column 1
+    //row:data,
+  }, {//column 2
+    //row:data,
+  }
+]);
+
