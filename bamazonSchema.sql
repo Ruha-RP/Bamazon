71fc6755a7cbcbfc2f3dd6fe8e8ae90a432ae583
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-15 12:43:24 -0500

    adds data to products

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -15,7 +15,7 @@ CREATE TABLE products (
 	item_id INT AUTO_INCREMENT NOT NULL,
 	product_name VARCHAR(100) NOT NULL,
 	department_name VARCHAR(100),
-	price DECIMAL(6,4) NOT NULL,
+	price DECIMAL NOT NULL,
 	stock_quantity INT NOT NULL,
 	PRIMARY KEY(item_id)
 );
---
+++ bamazonSeeds.sql
@@ -0,0 +1,18 @@
+--===================DATA======================
+ 
+ --slecting the database
+ USE bamazon_db;
+
+ --Inserting rows into the database
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+ ("Socks", "Clothing", 10.00, 1000),
+ ("Laptop", "Electronics", 750.00, 150),
+ ("Printer", "Electronics", 80.00, 50),
+ ("Pillow", "Home", 8.00, 500),
+ ("Book", "Hobbies", 2.50, 2000),
+ ("Kettle", "Home", 20.00, 300),
+ ("Fryer", "Home", 250.00, 150),
+ ("Diamond Ring", "Jewelry", 7800.00, 10),
+ ("Stroller", "Baby", 200.00, 90),
+ ("Quilt", "Home", 50.00, 43)
